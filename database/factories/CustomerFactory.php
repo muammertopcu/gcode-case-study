8219354cadb41735c939a9a89e66392cf37a9655
muammertopcu <muammertop@outlook.com>
2024-12-12 14:42:20 +0300

    Add Customer model, migration, factory, seeder, and controller

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'code' => $this->faker->unique()->regexify('[A-Z]{3}[0-9]{3}'),
+            'name' => $this->faker->company(),
+            'address' => $this->faker->address(),
+            'phone' => $this->faker->phoneNumber(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'created_by' => 1,
+            'updated_by' => 1,
+        ];
+    }
+}
